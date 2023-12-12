124029576c813a73dc0f7067a4e300f2054e3469
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-12 10:05:32 +0000

    new

---
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const converter = require('./10-converter').converter;
+
+let myConverter = converter(10);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
+
+myConverter = converter(16);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
+guillaume@ubuntu:~/0x13$ ./10-main.js
+2
+12
+89
+2
+c
+59
