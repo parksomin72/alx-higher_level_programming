d4288f703f8fcc7890e2baa7b01e2361a3a30df2
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-07 15:02:07 -0800

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,10 +5,14 @@ import sys
 if len(sys.argv) < 3:
     sys.exit(1)
 
-url = sys.argv[1]
+url = sys.argv[1].rstrip()
 email = sys.argv[2]
 
-payload = {'email': email}
-response = requests.post(url, data=payload)
+endpoint = '/post_email'
+full_url = f"{url.rstrip('/')}{endpoint}"
+
+data = {'email': email}
+response = requests.post(full_url, data=data)
 
 print("Your email is:", response.text)
+print("Full URL:", full_url)
