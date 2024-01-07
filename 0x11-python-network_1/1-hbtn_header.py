93771f54aa5e0dc31648d02994cd626df3274bb1
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-07 14:39:44 -0800

    .

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-
 if len(sys.argv) < 2:
-    sys.exit(1)
-
+sys.exit(1)
 url = sys.argv[1]
-
 with urllib.request.urlopen(url) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+x_request_id = response.headers.get('X-Request-Id')
+print(x_request_id)
