ef7dffb971118366382ad6865c58691156e35cff
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-07 14:46:24 -0800

    .

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+    print(body)
