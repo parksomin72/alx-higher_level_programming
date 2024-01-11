2f305b550c4da3ccd22a24a074e418656c6ecf09
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-11 13:49:41 -0800

    .

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays only the status code of the response
-
-URL=$1
-
-# Use curl to send a HEAD request and display only the HTTP status code
-curl -s -o /dev/null -w "%{http_code}" "$URL"
+curl -s -o /dev/null -w "%{http_code}" "$1"
