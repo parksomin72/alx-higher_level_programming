0c535716cc9bba2edb4d2830a391872e333995b6
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-11 15:07:37 -0800

    .

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script that makes a request to 0.0.0.0:5000/catch_me
+# This script makes a request to 0.0.0.0:5000/catch_me using curl
 
 # Make the request with curl, follow redirects, and set the user-agent header
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" http://0.0.0.0:5000/catch_me
+curl -sLX POST -H "Origin: HolbertonSchool" -d "user_id=98" http://0.0.0.0:5000/catch_me
