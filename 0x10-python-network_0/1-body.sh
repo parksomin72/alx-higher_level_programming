cb1c7d04fa512b051f4376914336b4d81d92fecf
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-11 06:34:19 -0800

    .

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Send a GET request to the URL and display the body of the response
-curl -sL -w "%{http_code}" "$1" -o /dev/null | {
-    read -r status
-    if [ "$status" -eq 200 ]; then
-        curl -sL "$1"
-    fi
-}
+# Send a GET request to the URL and display the body of the response for a 200 status code
+curl -sL "$1"
