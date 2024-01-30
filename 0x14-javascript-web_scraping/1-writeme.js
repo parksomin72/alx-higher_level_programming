c9306818f03c84526ff8e1fe63efaeda99533f0c
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-30 14:36:26 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Content successfully written to ${filePath}`);
+fs.writeFile(process.argv[2], process.argv[3], err => {
+	if (err) {
+    console.log(err);
   }
 });
