4c0d331bf057708de0d9dae8d8d9cade293dcec7
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-30 14:30:47 -0800

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content successfully written to ${filePath}`);
+  }
+});
