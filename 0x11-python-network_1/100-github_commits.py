77d09ccb307189374805ff8976014217f8406598
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-31 00:21:34 -0800

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""
-Query GitHub API for user's commits in a repository.
+"""query github api for user id
 """
 import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
+
 if __name__ == '__main__':
-    repository = argv[1]
-    owner = argv[2]
-
-    url = f'https://api.github.com/repos/{owner}/{repository}/commits'
-    response = requests.get(url, auth=HTTPBasicAuth(owner, argv[3]))
-
-    try:
-        commits = response.json()
-
-        if not commits:
-            print("No commits found.")
-            exit(0)
-
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
-
-    except ValueError:
-        print(f"Error: Unable to fetch commits. Status code: {response.status_code}")
+    r = requests.get(
+        'https://api.github.com/users/' + argv[1],
+        auth=HTTPBasicAuth(argv[1], argv[2])
+    )
+    print(r.json().get('id'))
