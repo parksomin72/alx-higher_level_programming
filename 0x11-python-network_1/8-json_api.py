cc7dddcb898abffd06e3409e6afa0e883ff17656
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-31 00:08:08 -0800

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
 import requests
 import sys
 
-if len(sys.argv) < 2:
-    q = ""
-else:
-    q = sys.argv[1]
+if __name__ == "__main__":
+    # Set the default value for the letter
+    q = "" if len(sys.argv) < 2 else sys.argv[1]
 
-url = 'http://0.0.0.0:5000/search_user'
-data = {'q': q}
+    # Make the POST request with the letter as a parameter
+    payload = {'q': q}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
-try:
-    response = requests.post(url, data=data)
-    json_data = response.json()
+    try:
+        # Try to parse the JSON response
+        data = response.json()
 
-    if json_data:
-        print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
-    else:
-        print("No result")
+        # Check if the JSON is not empty and properly formatted
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
 
-except ValueError:
-    print("Not a valid JSON")
+    except ValueError:
+        # Handle the case when the JSON is not properly formatted
+        print("Not a valid JSON")
