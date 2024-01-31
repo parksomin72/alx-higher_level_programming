7bec91b4ab461ba9d4334596a454d2f4b8e77303
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-30 23:39:52 -0800

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-import sys
+"""POST email to url
+"""
+from urllib import request, parse
+from sys import argv
 
-if len(sys.argv) < 3:
-    sys.exit(1)
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
-
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
-    print(body)
+if __name__ == '__main__':
+    url = argv[1]
+    args = parse.urlencode({'email': argv[2]}).encode('utf-8')
+    with request.urlopen(argv[1], args) as response:
+        print(response.read().decode('utf-8'))
