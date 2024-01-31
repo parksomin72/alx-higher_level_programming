d138ae249b2703fdf68df1dbc5f702901d200003
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-30 16:28:58 -0800

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL and displays the body of the response
-curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2"
-| grep -q 'Valid JSON' && echo "Valid JSON" || echo "Not a valid JSON"
+
+url=$1
+json_file=$2
+
+# Function to check if the JSON is valid
+is_valid_json() {
+  if python3 -m json.tool "$1" > /dev/null 2>&1; then
+    echo "Valid JSON"
+    return 0
+  else
+    echo "Not a valid JSON"
+    return 1
+  fi
+}
+
+# Check if the JSON is valid
+if is_valid_json "$json_file"; then
+  # Send POST request
+  response=$(
+    curl -sX POST \
+      -H "Content-Type: application/json" \
+      --data @"$json_file" \
+      "$url"
+  )
+  echo "$response"
+else
+  echo "Not a valid JSON"
+fi
